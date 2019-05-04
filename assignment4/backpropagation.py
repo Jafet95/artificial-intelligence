897804f27fc3e13c4d61e00f0965973e3d67fbe4
jafet95 <jafet.a15@gmail.com>
2019-05-04 10:36:44 -0600

    Add training data randomization

--- assignment4/backpropagation.py
+++ assignment4/backpropagation.py
@@ -51,7 +51,6 @@ class NN:
             for k in range(self.no):
                 self.wo[j][k] = rand(-2.0, 2.0)
 
-        # last change in weights for momentum
         self.ci = makeMatrix(self.ni, self.nh)
         self.co = makeMatrix(self.nh, self.no)
 
@@ -61,7 +60,6 @@ class NN:
 
         # input activations
         for i in range(self.ni-1):
-            #self.ai[i] = sigmoid(inputs[i])
             self.ai[i] = inputs[i]
 
         # hidden activations
@@ -80,18 +78,17 @@ class NN:
 
         return self.ao[:]
 
-
-    def backPropagate(self, targets, N, M):
+    def backPropagate(self, targets, eta):
         if len(targets) != self.no:
             raise ValueError('wrong number of target values')
 
-        # calculate error terms for output
+        # calculate error terms for output layer
         output_deltas = [0.0] * self.no
         for k in range(self.no):
             error = targets[k]-self.ao[k]
             output_deltas[k] = dsigmoid(self.ao[k]) * error
 
-        # calculate error terms for hidden
+        # calculate error terms for hidden layer
         hidden_deltas = [0.0] * self.nh
         for j in range(self.nh):
             error = 0.0
@@ -103,15 +100,14 @@ class NN:
         for j in range(self.nh):
             for k in range(self.no):
                 change = output_deltas[k]*self.ah[j]
-                self.wo[j][k] = self.wo[j][k] + N*change + M*self.co[j][k]
+                self.wo[j][k] = self.wo[j][k] + eta*change
                 self.co[j][k] = change
-                #print N*change, M*self.co[j][k]
 
         # update input weights
         for i in range(self.ni):
             for j in range(self.nh):
                 change = hidden_deltas[j]*self.ai[i]
-                self.wi[i][j] = self.wi[i][j] + N*change + M*self.ci[i][j]
+                self.wi[i][j] = self.wi[i][j] + eta*change
                 self.ci[i][j] = change
 
         # calculate error
@@ -134,18 +130,29 @@ class NN:
         for j in range(self.nh):
             print(self.wo[j])
 
-    def train(self, patterns, iterations=1000, N=0.5, M=0.1):
-        # N: learning rate
-        # M: momentum factor
-        for i in range(iterations):
+    def writeOutput(self,output_data):
+        with open(output_data,'w') as f:
+            for line in self.wi:
+                np.savetxt(f, line, fmt='%.sf')
+
+    def train(self, input_data, output_data, max_iterations=1000, eta=0.5, min_error=1.0):
+        for i in range(max_iterations):
             error = 0.0
-            for p in patterns:
+            # Randomly shuffle the training data
+            np.take(input_data,np.random.permutation(input_data.shape[0]),axis=0,out=input_data)
+            for p in input_data:
                 inputs = p[0]
                 targets = p[1]
                 self.update(inputs)
-                error = error + self.backPropagate(targets, N, M)
+                error = error + self.backPropagate(targets, eta)
             if i % 100 == 0: #Print error only iterations/100 times
                 print('error %-.5f' % error)
+            if error < min_error:
+                print ("Reached minimum error criteria")
+                break
+        print ("Reached max iterations")
+        # Create output file with the weights
+        # ~ self.writeOutput(output_data)
 
 
 def demo():
@@ -193,12 +200,19 @@ def demo():
         0,1,1,0,0,0,1,1,0,0,1,1,1,1,1,0], [0,0,0,0,0,0,0,0,0,1]]
     ]
 
-    # create a basic NN (3 layers)
+    #Convert to NumPy array
+    data_base = np.array(data_base)
+
+    test_case = np.copy(data_base)
+    np.take(test_case,np.random.permutation(test_case.shape[0]),axis=0,out=test_case)
+
+    # Create a basic NN (3 layers)
+    # NN(self, #nodos de entrada, #nodos de la capa oculta, #nodos de salida)
     n = NN(len(data_base[0][0]), len(data_base[0][0]), len(data_base[0][1]))
-    # train it with some patterns
-    n.train(data_base)
-    # test it
-    n.test(data_base)
+    # Train it with some patterns
+    n.train(data_base, "output.txt", 1000, 0.5, 0.001)
+    # Test it
+    n.test(test_case)
 
 if __name__ == '__main__':
     demo()
